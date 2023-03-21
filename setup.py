50f947abd323f7a443caab49f438386e9adb8158
Marc Lehmann <marc@navisense.de>
2023-03-21 06:23:11 +0100

    Upgrade Python

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ with (requirements_path / 'dev.txt').open() as f:
 setuptools.setup(
     name='port-emission-inventory', version='0.1',
     description='Tools to create emission inventories for ports.',
-    python_requires='>=3.10', install_requires=requirements,
+    python_requires='>=3.11', install_requires=requirements,
     extras_require={'test': test_requirements, 'dev': dev_requirements},
     license='AGPL-3.0-or-later')
